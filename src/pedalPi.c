5056c8bca56fbe03d1cb5ec96563b53e682909b2
gilbertaoe <gilbertaoe@hotmail.com>
2019-03-17 15:27:18 -0500

    Update pedalPi.c

--- src/pedalPi.c
+++ src/pedalPi.c
@@ -1,7 +1,16 @@
+#include <bcm2835.h>
 #include <stdio.h>
 
-int main()
+int main(int argc, char **argv)
 {
-    printf("Hello World!");
-    return 0;
+	if(!bcm2835_init())
+	{
+		printf("Failed to init!\n");
+
+		return -1;
+	}
+
+	printf("Success!\n");
+
+	return 0;
 }
